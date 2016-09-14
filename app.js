e83a601805d4381de337b7f1e7f03b2ba4c8fe15
othman853 <anuar.yasser@gmail.com>
2016-09-14 14:49:47 -0300

    Separating app creating logic to a dedicated file

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+
+routes.forEach((route) => app.use(route));
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./routes');
-
-const app = express();
-
-app.use(morgan('tiny'));
-app.use(bodyParser.json());
-
-routes.forEach((route) => app.use(route));
-
+const app = require('./app');
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log(`Up on port: ${port}`));
