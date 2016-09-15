b1ca1e2bebf653434be81de8ca1cef21b18d454f
othman853 <anuar.yasser@gmail.com>
2016-09-15 16:23:23 -0300

    Using logger on index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const app = require('./app');
+const db = require('./models/db-connection.js');
+const logger = require('./loggers/logger')('index');
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Up on port: ${port}`));
+app.listen(port, () => logger.info(`Up on port: ${port}`));
