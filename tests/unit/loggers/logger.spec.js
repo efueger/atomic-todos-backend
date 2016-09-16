6a5996e854a59bb90947b951393ec8696159ecff
othman853 <anuar.yasser@gmail.com>
2016-09-16 15:29:29 -0300

    Adding unit tests for logger

--- tests/unit/loggers/logger.spec.js
+++ tests/unit/loggers/logger.spec.js
@@ -1,23 +1,59 @@
+
+'use strict';
+
 const chalk = require('chalk');
 const chai = require('chai');
+const chaiString = require('chai-string');
 const path = require('path');
 const loggerBuilder = require(path.join(process.cwd(), 'loggers', 'logger'));
-
+const stdout = require('test-console').stdout;
 const assert = chai.assert;
+const expect = chai.expect;
+chai.use(chaiString);
 
 describe(chalk.magenta('Unit: Logger'), () => {
 
-  it.skip('Should have the tag [TEST]', (done) => {
-    const logger = loggerBuilder('test');
+  const logger = loggerBuilder('test');
 
-    logger.info('a log');
-    logger.error('a log');
+  it('Should have the tag [TEST]', (done) => {
 
-    process.stdout.on('data', (data) => {
-      console.log(`Data: ${data}`);
-      output.end();
-      done();
+    const output = stdout.inspectSync( () => {
+      logger.info('a log');
     });
+
+    expect(output[0]).to.have.entriesCount("[TEST]", 1);
+    done();
+  });
+
+  it('Should have the type \'info:\' ', (done) => {
+
+    const output = stdout.inspectSync( () => {
+      logger.info('a log');
+    });
+
+    expect(output[0]).to.have.entriesCount("info:", 1);
+
+    done();
+  });
+
+  it('Should log the [TEST] tag using the magenta color', (done) => {
+
+    const output = stdout.inspectSync( () => {
+      logger.info('a log');
+    });
+
+    expect(output[0]).to.have.entriesCount("\u001b[35m[TEST]\u001b[39m", 1);
+    done();
+  });
+
+  it('Should contain the \'a log\' logged message', (done) => {
+
+    const output = stdout.inspectSync( () => {
+      logger.info('a log');
+    });
+
+    expect(output[0]).to.have.entriesCount("a log", 1);
+    done();
 
   });
 
