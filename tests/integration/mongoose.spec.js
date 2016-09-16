4b1bb20f397b86d1183abcb30ff18dc26ddb7409
othman853 <anuar.yasser@gmail.com>
2016-09-16 15:29:19 -0300

    Adding mongo integration tests

--- tests/integration/mongoose.spec.js
+++ tests/integration/mongoose.spec.js
@@ -3,20 +3,45 @@ const path = require('path');
 const chalk = require('chalk');
 const mongoose = require('mongoose');
 
-require(path.join(process.cwd(), 'models', 'db-connection'));
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+
+const connection = require(path.join(process.cwd(), 'models', 'db-connection'));
+
 
 describe(chalk.magenta('Integration: Mongoose'), () => {
 
-  it('Should save an object in a mongo database', (done) => {
+  const IntegrationTestDocument = mongoose.model('IntegrationTestDocument', {
+    test: String
+  });
 
-    const IntegrationTestDocument = mongoose.model('IntegrationTestDocument', {
-      test: String
-    });
+  it('Should connect to a mongo database', (done) => {
+    const onConnectCallback = done;
+    const onErrorCallback = done;
+    connection(onConnectCallback, onErrorCallback);
+  });
+
+  it('Should save an object in a mongo database', (done) => {
 
     const testDocument = new IntegrationTestDocument({ test: 'Testing' });
 
     testDocument.save(function(error) {
       return error ? done(error) : done();
+    });
+
+  });
+
+  it('Should retrieve an object from the database', (done) => {
+
+    IntegrationTestDocument.findOne({test:'Testing'}, (error, doc) => {
+
+      if (error) {
+        return done(error);
+      }
+
+      expect(doc).to.have.property('test').and.equal('Testing');
+      done();
     });
 
   });
