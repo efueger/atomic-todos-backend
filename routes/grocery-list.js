ef0937e6fc914bac5e40f063a3155804973bcf51
othman853 <anuar.yasser@gmail.com>
2016-09-21 12:09:35 -0300

    Changing the call of actions in routes

--- routes/grocery-list.js
+++ routes/grocery-list.js
@@ -4,18 +4,19 @@ const path = require('path');
 
 const pathForAction = (actionName) => path.join(process.cwd(), 'actions', 'grocery-list', actionName);
 
-const rootAction = require(pathForAction('root'))();
-const createAction = require(pathForAction('create'))();
+const rootAction = require(pathForAction('root'));
+const createAction = require(pathForAction('create'));
 const Router = require('express').Router;
 
-module.exports = (groceryStoreList, routerConfig) => {
+module.exports = (GroceryStoreList, routerConfig) => {
 
   routerConfig = routerConfig || require(path.join(process.cwd(), 'etc', 'config')).routerConfig;
 
   const router = Router(routerConfig);
+  const context = { GroceryStoreList: GroceryStoreList };
 
-  router.get('/', new RootAction(groceryStoreList).apply);
-  router.post('/', new CreateAction(groceryStoreList).apply);
+  router.get('/', rootAction.bind(context));
+  router.post('/', createAction.bind(context));
 
   return router;
 };
