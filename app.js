533af5faa6c6e3f6397a138f46a77fd6435cbd37
othman853 <anuar.yasser@gmail.com>
2016-09-23 18:09:40 -0300

    Dynamically loading routes and models into app object

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
+'use strict';
+
+const path = require('path');
+const camelCase = require('uppercamelcase');
+const fileSystem = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes');
-
+const camelcase = require('camelcase');
+const groceryRouterFactory = require('./routes/grocery-list');
+const logger = require('./loggers/logger')('setup');
 const app = express();
+
+const loadModels = (modelsPath, app) => {
+  fileSystem.readdirSync(modelsPath).forEach(file => {
+    const modelName = camelCase(file.replace('.js', ''));
+    logger.info(`Model: ${modelName}`);
+    app.models[modelName] = require(path.join(modelsPath, file));
+  });
+};
+
+const loadRoutes = (routesPath, app) => {
+  fileSystem.readdirSync(routesPath).forEach(file => {
+    let route = require(path.join(routesPath, file))(app);
+    route.router.stack.forEach(stackItem => {
+      stackItem.route.stack.forEach(innerStackItem => {
+        logger.info(`Route: ${innerStackItem.method.toUpperCase()} ${path.join(route.prefix, stackItem.route.path)}`);
+      });
+    });
+
+    app.use(route.prefix, route.router);
+  });
+};
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
-routes.forEach((route) => app.use(route.prefix, route.router));
+app.models = {};
+
+const modelsRootPath = path.join(process.cwd(), 'models');
+const routesRootPath = path.join(process.cwd(), 'routes');
+
+loadModels(modelsRootPath, app);
+loadRoutes(routesRootPath, app);
 
 module.exports = app;
