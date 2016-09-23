cda52bbd25adb7fdb87746dba88635dfd534590d
othman853 <anuar.yasser@gmail.com>
2016-09-23 11:16:15 -0300

    Removing RouterConfig param of route

--- routes/grocery-list.js
+++ routes/grocery-list.js
@@ -3,14 +3,13 @@
 const path = require('path');
 
 const pathForAction = (actionName) => path.join(process.cwd(), 'actions', 'grocery-list', actionName);
+const routerConfig = require(path.join(process.cwd(), 'etc', 'config')).routerConfig;
 
 const rootAction = require(pathForAction('root'));
 const createAction = require(pathForAction('create'));
 const Router = require('express').Router;
 
-module.exports = (GroceryStoreList, routerConfig) => {
-
-  routerConfig = routerConfig || require(path.join(process.cwd(), 'etc', 'config')).routerConfig;
+module.exports = (GroceryStoreList) => {
 
   const router = Router(routerConfig);
   const context = { GroceryStoreList: GroceryStoreList };
