7d2dbf72e0d38b7df95a99ab4dcb4d2cbe38647d
othman853 <anuar.yasser@gmail.com>
2016-09-24 02:57:35 -0300

    Adding tests to middleware

--- middlewares/content-type-checker.js
+++ middlewares/content-type-checker.js
@@ -5,11 +5,13 @@ module.exports = (request, response, next) => {
   const requestContentType = request.headers['content-type'];
 
   if ( (typeof requestContentType === 'undefined') || (requestContentType === null)) {
-    return response.status(400).send('Content-Type header is not set for this request');
+    response.status(400).send('Content-Type header is not set for this request');
+    return next();
   }
 
   if (requestContentType.indexOf('application/json') === -1) {
-    return response.status(400).send('Request Content-Type header is invalid.');
+    response.status(400).send('Request Content-Type header is invalid. It must be only application/json');
+    return next();
   }
 
   return next();
---
+++ tests/unit/middlewares/content-type-cheker.spec.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const contentTypeChecker = require(path.join(process.cwd(), 'middlewares', 'content-type-checker'));
+
+describe(chalk.magenta('Unit: Middleware: Content Type Checker'), () => {
+
+  it('Should call next when request has Content-Type equals to application/json', (done) => {
+
+    const expectedContentType = 'application/json';
+
+    const request = { headers: { 'content-type': expectedContentType } };
+    const response = {};
+
+    contentTypeChecker(request, response, done);
+
+  });
+
+  it('Should respond with 400 and an error message when Content-Type is absent from request headers', (done) => {
+
+    const request = { headers: {} };
+    const response = {};
+
+    response['send'] = sinon.stub();
+    response['status'] = sinon.stub().returns(response);
+
+    contentTypeChecker(request, response, () => {
+      expect(response.status).to.have.been.calledWith(400);
+      expect(response.send).to.have.been.calledWith('Content-Type header is not set for this request');
+      done();
+    });
+
+  });
+
+  it('Should respond with 400 and an error message when Content-Type request header is different from application/json', (done) => {
+
+    const request = { headers: {'content-type': 'whatever'} };
+    const response = {};
+
+    response['send'] = sinon.stub();
+    response['status'] = sinon.stub().returns(response);
+
+    contentTypeChecker(request, response, () => {
+      expect(response.status).to.have.been.calledWith(400);
+      expect(response.send).to.have.been.calledWith('Request Content-Type header is invalid. It must be only application/json');
+      done();
+    });
+
+  });
+
+  it('Should respond with 400 and an error message when Content-Type request header is null or undefined', (done) => {
+
+    const request = { headers: {'content-type': undefined} };
+    const response = {};
+
+    response['send'] = sinon.stub();
+    response['status'] = sinon.stub().returns(response);
+
+    contentTypeChecker(request, response, () => {
+      expect(response.status).to.have.been.calledWith(400);
+      expect(response.send).to.have.been.calledWith('Content-Type header is not set for this request');
+      done();
+    });
+
+
+  });
+
+});
