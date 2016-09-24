6ef7402e8bcc24edab9f6e0ca0ce4f30ba8e6591
othman853 <anuar.yasser@gmail.com>
2016-09-24 02:01:40 -0300

    First e2e test is passing locally

--- tests/e2e/grocery-list.js
+++ tests/e2e/grocery-list.js
@@ -3,8 +3,12 @@
 const app = require(path.join(process.cwd(), 'app'))();
 
 const request = supertest(app);
+const db = require(path.join(process.cwd(), 'etc', 'database'));
 
 describe(chalk.magenta('End to end: Grocery List'), () => {
+
+  before(() => db.connect({ url: process.env.ATOMIC_MONGO_TEST_URL }));
+  after(() => db.disconnect());
 
   it('Should create a new GroceryList on POST /grocery-lists/', (done) => {
 
