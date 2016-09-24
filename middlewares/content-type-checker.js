13774a6f15d6fb91775e3ede839f01763c4d319b
othman853 <anuar.yasser@gmail.com>
2016-09-24 02:37:44 -0300

    Adding a content type checker middleware

---
+++ middlewares/content-type-checker.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = (request, response, next) => {
+
+  const requestContentType = request.headers['content-type'];
+
+  if ( (typeof requestContentType === 'undefined') || (requestContentType === null)) {
+    return response.status(400).send('Content-Type header is not set for this request');
+  }
+
+  if (requestContentType.indexOf('application/json') === -1) {
+    return response.status(400).send('Request Content-Type header is invalid.');
+  }
+
+  return next();
+
+}
