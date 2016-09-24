ad92274e33983815f9676d6652ac20e3e1c226a4
othman853 <anuar.yasser@gmail.com>
2016-09-24 01:45:15 -0300

    Refactoring app creation logic

--- app.js
+++ app.js
@@ -9,19 +9,21 @@ const morgan = require('morgan');
 const camelcase = require('camelcase');
 const groceryRouterFactory = require('./routes/grocery-list');
 const logger = require('./loggers/logger')('setup');
-const app = express();
+const contentTypeChecker = require('./middlewares/content-type-checker');
+
+const removeJsExtension = (fileName) => fileName.replace('.js', '');
 
 const loadModels = (modelsPath, app) => {
-  fileSystem.readdirSync(modelsPath).forEach(file => {
-    const modelName = camelCase(file.replace('.js', ''));
+  fileSystem.readdirSync(modelsPath).forEach(modelFile => {
+    const modelName = camelCase(removeJsExtension(modelFile));
     logger.info(`Model: ${modelName}`);
-    app.models[modelName] = require(path.join(modelsPath, file));
+    app.models[modelName] = require(path.join(modelsPath, modelFile));
   });
 };
 
 const loadRoutes = (routesPath, app) => {
-  fileSystem.readdirSync(routesPath).forEach(file => {
-    let route = require(path.join(routesPath, file))(app);
+  fileSystem.readdirSync(routesPath).forEach(routeFile => {
+    let route = require(path.join(routesPath, routeFile))(app);
     route.router.stack.forEach(stackItem => {
       stackItem.route.stack.forEach(innerStackItem => {
         logger.info(`Route: ${innerStackItem.method.toUpperCase()} ${path.join(route.prefix, stackItem.route.path)}`);
@@ -32,15 +34,28 @@ const loadRoutes = (routesPath, app) => {
   });
 };
 
-app.use(morgan('tiny'));
-app.use(bodyParser.json());
+const appFactory = () => {
 
-app.models = {};
 
-const modelsRootPath = path.join(process.cwd(), 'models');
-const routesRootPath = path.join(process.cwd(), 'routes');
+  const app = express();
 
-loadModels(modelsRootPath, app);
-loadRoutes(routesRootPath, app);
+  app.use(morgan('tiny'));
+  app.use(bodyParser.json());
+  app.use(contentTypeChecker);
 
-module.exports = app;
+  app.models = {};
+
+  const modelsRootPath = path.join(process.cwd(), 'models');
+  const routesRootPath = path.join(process.cwd(), 'routes');
+
+  loadModels(modelsRootPath, app);
+  loadRoutes(routesRootPath, app);
+
+  const db = require('./etc/database')({ url: process.env.ATOMIC_MONGO_URL });
+
+  return app;
+
+};
+
+
+module.exports = appFactory;
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const port = process.env.PORT || 3000;
-const app = require('./app');
-const db = require('./etc/database')({ url: process.env.ATOMIC_MONGO_URL });
+const app = require('./app')();
 const logger = require('./loggers/logger')('index');
 
 app.listen(port, () => logger.info(`Up on port: ${port}`));
