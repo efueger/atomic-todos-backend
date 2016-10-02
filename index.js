d46112edbcf49256d387adcbe15b492f00276ec1
othman853 <anuar.yasser@gmail.com>
2016-10-02 02:01:10 -0300

    Shutdown application when db connection fails on startup

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const app = require('./app')();
 const logger = require('./loggers/console-logger')('index');
 const db = require('./etc/database');
 
-db.connect({ url: process.env.ATOMIC_MONGO_URL });
+db.connect({
+  url: process.env.ATOMIC_MONGO_URL,
+  onDisconnect() {
+    logger.error('Could not connect to database on startup');
+    process.exit(0)
+  }
+});
+
 app.listen(port, () => logger.info(`Up on port: ${port}`));
