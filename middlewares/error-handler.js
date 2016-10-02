165eb17dc9d125406ecdd1c9ff8c63c45adc04d1
othman853 <anuar.yasser@gmail.com>
2016-10-02 02:03:44 -0300

    Error handler will now use new error structure

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,8 +1,9 @@
 'use strict';
 
 module.exports = (error, request, response, next) => {
-  if (error.status && error.message) {
-    return response.status(error.status).json({message: error.message});
+
+  if (error.status) {
+    return response.status(error.status).json(error.body);
   }
 
   return response.status(500).json({message: 'Internal Generic Error'});
