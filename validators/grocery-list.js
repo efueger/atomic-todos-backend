445e624dad4b3d048138f3751d60659fa64e13da
othman853 <anuar.yasser@gmail.com>
2016-10-02 02:03:19 -0300

    Refactoring validator

--- validators/grocery-list.js
+++ validators/grocery-list.js
@@ -1,13 +1,14 @@
 const exists = require('exists');
 
 module.exports = {
-  create:  (request) => {
+
+  create(request) {
 
     if (!exists(request.body)){
       return new Error('Request body is empty');
     }
 
-    if (! (request.body instanceof Array) ) {
+    if (!(request.body instanceof Array)) {
       return new Error('Request body is not an array');
     }
 
