6992ef517724abaa9b8d52fbd38bd1cbec62b72a
othman853 <anuar.yasser@gmail.com>
2016-10-13 18:39:03 -0300

    Adding router log tag

--- app/loggers/tags.js
+++ app/loggers/tags.js
@@ -1,6 +1,7 @@
 module.exports = {
   MONGO: { text: 'mongo', enabled: true },
   INDEX: { text: 'index', enabled: true },
+  ROUTE: { text: 'route', enabled: true },
   SETUP: { text: 'setup', enabled: true },
   DEFAULT: { text: 'no-tag', enabled: true}
 };
