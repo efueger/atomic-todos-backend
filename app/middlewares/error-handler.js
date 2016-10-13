a2e7a5ec8f3d18e78ee4fb306ca17784f97a0e41
othman853 <anuar.yasser@gmail.com>
2016-10-13 18:42:54 -0300

    Adopting http constants on error handler

--- app/middlewares/error-handler.js
+++ app/middlewares/error-handler.js
@@ -1,8 +1,10 @@
+const INTERNAL_SERVER_ERROR = require('../etc/http').STATUS_CODE.INTERNAL_SERVER_ERROR;
+
 module.exports = (error, request, response, next) => {
 
   if (error.status) {
     return response.status(error.status).json(error.body);
   }
 
-  return response.status(500).json({ message: 'Internal Generic Error' });
+  return response.status(INTERNAL_SERVER_ERROR).json({ message: 'Internal Generic Error' });
 };
