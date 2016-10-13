68c3584fef5e57df08f7ad5276d8a3ae89a3c97b
othman853 <anuar.yasser@gmail.com>
2016-10-13 18:36:58 -0300

    Adding some validations and loggings to Action

--- app/actions/base-action.js
+++ app/actions/base-action.js
@@ -1,21 +1,27 @@
 
 'use strict';
 
-const METHODS = {
-  GET: 'get',
-  POST: 'post'
-};
+const loggers = require('../loggers');
+const logger = loggers.consoleLogger(loggers.tags.ROUTE);
 
 class Action {
 
-  constructor(app) {
-    this.app = app;
-  }
+  constructor(app) { this.app = app; }
 
-  register(endpoint, method) {
+  register(method, endpoint) {
+
+    if ( !this.handle) {
+      throw new Error(`handle(req, res, next) not implemented on ${ this.constructor.name }`);
+    }
+
+    if (this.pre) {
+      this.app[method](endpoint, this.pre.bind(this));
+    }
+
+    logger.info(`${method.toUpperCase()} ${endpoint} - ${this.constructor.name}`);
     this.app[method](endpoint, this.handle.bind(this));
   }
 
 }
 
-module.exports = { Action, METHODS };
+module.exports = Action;
---
+++ test/unit/actions/base-action.spec.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const Action = require('../../../app/actions/base-action');
+
+describe('Unit: Action: Base Action', () => {
+
+  let app = {};
+  const METHOD = 'method';
+
+  app[METHOD] = sinon.stub();
+  const handleStub = function () {};
+  const handleBindStub = sinon.stub();
+  const preStub = function () {};
+  const preBindStub = sinon.stub();
+
+  handleStub.bind = sinon.stub().returns(handleBindStub);
+  preStub.bind = sinon.stub().returns(preBindStub);
+
+  beforeEach( done => {
+    app.method.reset();
+    handleStub.bind.reset();
+    handleBindStub.reset();
+    done();
+  });
+
+  it('Should register the handle method in the app', done => {
+
+    const expectedEndpoint = 'endpoint';
+    const action = new Action(app);
+    action.handle = handleStub;
+
+    action.register(METHOD, expectedEndpoint);
+
+    expect(app[METHOD]).to.have.been.calledWith(expectedEndpoint, handleBindStub);
+    expect(action.handle.bind).to.have.been.calledWith(action);
+    done();
+  });
+
+  it('Should register the pre method in the app', done => {
+
+    const expectedEndpoint = 'endpoint';
+    const action = new Action(app);
+    action.handle = handleStub;
+    action.pre = preStub;
+
+    action.register(METHOD, expectedEndpoint);
+
+    expect(app[METHOD]).to.have.been.calledWith(expectedEndpoint, preBindStub);
+    expect(action.pre.bind).to.have.been.calledWith(action);
+
+    done();
+  });
+
+  it('Should throw an error when no handle is present', done => {
+    const action = new Action(app);
+
+    expect(() => action.register()).to.throw(`handle(req, res, next) not implemented on ${ action.constructor.name }`);
+    done();
+  });
+});
