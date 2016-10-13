1158e3f561e0c4a3c3e7c77246fd8aeca58d1320
othman853 <anuar.yasser@gmail.com>
2016-10-13 20:22:45 -0300

    Implementing tests to action register module

--- test/unit/etc/action-register.spec.js
+++ test/unit/etc/action-register.spec.js
@@ -1,5 +1,32 @@
+'use strict';
+
+const register = require('../../../app/etc/action-register');
 
 describe('Unit: Etc: Action Register', () => {
+  const expectedApp = {};
+  const expectedMethod = 'get';
+  const expectedEndpoint = '/some-endpoint';
 
+  it('Should call action constructor passing app and register', done => {
+    const action = { Constructor: ActionMock, method: expectedMethod, endpoint: expectedEndpoint };
+    const actionList = [action];
+
+    register.registerAll(expectedApp, actionList);
+
+    done();
+
+  });
+
+
+  class ActionMock {
+    constructor(app) {
+      expect(app).to.deep.equal(expectedApp);
+    }
+
+    register(method, endpoint) {
+      expect(method).to.equal(expectedMethod);
+      expect(endpoint).to.equal(expectedEndpoint);
+    }
+  }
 
 });
