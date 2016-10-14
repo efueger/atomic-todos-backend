c3089adee2cc998595dba7e5f19f80d4b7c04402
othman853 <anuar.yasser@gmail.com>
2016-10-14 02:34:49 -0300

    Adding more logger tags

--- app/loggers/tags.js
+++ app/loggers/tags.js
@@ -1,7 +1,9 @@
 module.exports = {
-  MONGO: { text: 'mongo', enabled: true },
+  APP: { text: 'app', enabled: true },
+  DEFAULT: { text: 'no-tag', enabled: true },
   INDEX: { text: 'index', enabled: true },
+  MIDDLEWARE: { text: 'middleware', enabled: true },
+  MONGO: { text: 'mongo', enabled: true },
   ROUTE: { text: 'route', enabled: true },
-  SETUP: { text: 'setup', enabled: true },
-  DEFAULT: { text: 'no-tag', enabled: true}
+  SETUP: { text: 'setup', enabled: true }
 };
