96097d8f6ca31b711c535099281e12a168f49fce
othman853 <anuar.yasser@gmail.com>
2016-10-17 14:27:48 -0200

    Adding PORT and using ENVS enum on env

--- app/etc/env.js
+++ app/etc/env.js
@@ -1,5 +1,9 @@
+
+const envs = require('./environments');
+
 module.exports = {
-  ENV: process.env.NODE_ENV || 'dev',
+  ENV: process.env.NODE_ENV || envs.DEV,
   DATABASE_URL: process.env.ATOMIC_MONGO_URL,
+  PORT: process.env.PORT || 3000,
   TEST_DATABASE_URL: process.env.ATOMIC_TEST_MONGO_URL
 };
